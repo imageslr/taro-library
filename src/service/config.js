191bd7a2cb2743b378affa48b7f6c18362eae2b8
elonz <613366667@qq.com>
2021-04-27 16:53:56 +0800

    chores: update mock api
    Change-Id: I9ffaf20e005b90a98678e17659ef5c8434737643

--- src/service/config.js
+++ src/service/config.js
@@ -1,5 +1,5 @@
 const devURL = "http://localhost:3000"; // 开发环境，需要开启 mock server（执行：gulp mock）
-const prodURL = "TODO"; // 生产环境，线上服务器
+const prodURL = "http://mock.51y.cc:81/mock/6087977be290eb06c92f1ae9"; // 生产环境，线上服务器
 
 const BASE_URL = process.env.NODE_ENV === "development" ? devURL : prodURL;
 
